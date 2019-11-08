04b36a9aed8fee18575345ae3558fc098c9b411f
Ujjwal Jindal <jindalu@amazon.com>
2019-11-09 01:58:52 +0530

    first commit

---
+++ src/main/java/com/example/HajratBackend/HajratBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.HajratBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HajratBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HajratBackendApplication.class, args);
+	}
+
+}
